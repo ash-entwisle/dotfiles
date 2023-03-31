ba4f0a83d1a838aa04127a83c467b1576d87752e
Ash Entwisle <ash.entwisle@mailfence.com>
2023-03-31 18:16:24 +0100

    initial commit
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+abbr --add 'la' 'ls -al'
+abbr --add 'ksh' 'kitty +kitten ssh' 
+abbr --add 'hxfsh' 'hx ~/.config/fish'
+abbr --add 'cls' 'clear'
