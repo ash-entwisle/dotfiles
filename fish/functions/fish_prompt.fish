38fb4447f532176be525288ac9e3f2e9246e6cff
Ash Entwisle <ash.entwisle@mailfence.com>
2023-03-31 20:05:54 +0100

    updated prompt
    Signed-off-by: Ash Entwisle <ash.entwisle@mailfence.com>

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,16 +7,16 @@ function fish_prompt --description 'Write out the prompt'
     set -l prompt_status ""
 
     # Since we display the prompt on a new line allow the directory names to be longer.
-    set -q fish_prompt_pwd_dir_length
-    or set -lx fish_prompt_pwd_dir_length 0
+#    set -q fish_prompt_pwd_dir_length
+#    or set -lx fish_prompt_pwd_dir_length 0
 
     # Color the prompt differently when we're root
-    set -l suffix '❯'
+    set -l suffix '>>>'
     if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set cwd_color (set_color $fish_color_cwd_root)
         end
-        set suffix '#'
+        set suffix '#>>'
     end
 
     # Color the prompt in red on error
@@ -25,6 +25,6 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_status $status_color "[" $last_status "]" $normal
     end
 
-    echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
-    echo -n -s $status_color $suffix ' ' $normal
+    echo -s 'ash@' (prompt_hostname) ' ' $cwd_color (basename $PWD) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -n -s (set_color brgreen) $suffix ' ' $normal
 end
